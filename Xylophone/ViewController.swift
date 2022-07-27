56d0ca7c124753fc489f24e901072ce15b5ceff0
Ben Cockin <53189653+Bencockin@users.noreply.github.com>
2022-07-27 15:20:30 +0100

    Swift practice project

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        //Dim button
+        sender.alpha = 0.3
+        
+        //0.2s Delay undim button
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            UIView.animate(withDuration: 0.3){
+              //Bring's sender's opacity back up to fully opaque.
+              sender.alpha = 1.0
+                
+            }
+        }
+        //play sound
+        playSound(soundName: sender.currentTitle!)
+    }
 
     
-
+    
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
